9818db947a1d0face89318c2d442a7a167437480
radu <radu@skxc.tech>
2020-01-12 18:54:16 +0200

    should handle class perks somehow

---
+++ lib/models/brute.dart
@@ -0,0 +1,34 @@
+import 'package:ghcharacter/models/character.dart';
+
+class Brute extends Character {
+  Brute(String name, String playableClass, int xp, int gold) : super(name, playableClass, xp, gold);
+
+  int get maxHp {
+    if (this.level == 1) {
+      return 10;
+    } else {
+      return 8 + this.level * 2;
+    }
+  }
+
+  // TODO handle this logic after building
+  // attack modifier deck logic
+  List<String> perks = [
+    'Remove two -1 cards',
+    'Replace one -1 card with one + 1 card',
+    'Add two +1 cards',
+    'Add two +1 cards',
+    'Add one +3 card',
+    'Add three PUSH 1 cards',
+    'Add three PUSH 1 cards',
+    'Add two PIERCE 3 cards',
+    'Add one STUN card',
+    'Add one STUN card',
+    'Add one DISARM card and one MUDDLE card',
+    'Add one ADD TARGET card',
+    'Add one ADD TARGET card',
+    'Add one +1 Shield 1, Self card',
+    'Ignore negative item effects and add one +1 card'
+  ];
+
+}
